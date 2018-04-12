325bf273b14968fb2c90475e9bd2175f8188496b
Alejok28 <alejo_k28@hotmail.com>
2018-04-12 17:15:43 -0500

    Adds portfolio and About me section

--- js/app.js
+++ js/app.js
@@ -32,7 +32,45 @@ $(document).ready(function () {
       $(document).on("scroll", onScroll);
     });
   });
+
+  // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+// var btn = document.getElementById("link");
+// console.log(btn.dataset.src);
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+$('div#link').on('click', function(){
+  modal.style.display = "block";
+  var img = this.dataset.src;
+  var title = this.dataset.title;
+  var p =this.dataset.text
+  var url = this.dataset.url;
+  $(".modal-content").append("<h3>"+title+"</h3>");
+  $(".modal-content").append("<img src='" + img + "'>");
+  $(".modal-content").append("<p class='description'>TECNOLOGÍAS UTILIZADAS: "+p+"</p>")
+  $(".modal-content").append('<a class="go-link"href="'+ url +'" target="_blank">Vistar</a>')
+})
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+//   $('.modal-content').empty();
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+      $('.modal-content').empty();
+  }
+}
 });
+
 
 function onScroll(event){
   var scrollPosition = $(document).scrollTop();
